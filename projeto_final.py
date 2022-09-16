04537830654cde9d349517e1a3ac2d7e93ab1643
Ettore Shiniti Mitsueda <ettore.mitsueda@luizalabs.com>
2022-09-16 11:10:31 -0300

    Padronizar referências à lista de produtos e ao carrinho no menu do carrinho

--- projeto_final.py
+++ projeto_final.py
@@ -7,14 +7,35 @@ os.system("cls")
 # (Ettore) lista de produtos cadastrados
 produtos = [
     {
-        "nome": "Arroz",
-        "preço": 5
+        "nome": "Jenipapo",
+        "preço": 25
     },
     {
-        "nome": "Feijão",
-        "preço": 4
-    }
+        "nome": "Lichia",
+        "preço": 28
+    },
+    {
+        "nome": "Pitaya",
+        "preço": 30
+    },
+    {
+        "nome": "Kiwi",
+        "preço": 24
+    },
+    {
+        "nome": "Goiaba",
+        "preço": 12
+    },
+    {
+        "nome": "Pitanga",
+        "preço": 14
+    },
+    {
+        "nome": "Café",
+        "preço": 20
+    },
 ]
+carrinho = []
 
 # (Ettore) função para listar produtos cadastrados
 def lista_produtos():
@@ -370,66 +391,51 @@ while flag_menu_navegação:
 
                 print(data_hora.strftime("%d/%m/%Y")+" "*espaço3+data_hora.strftime("%H:%M"),"\n")
 
+                lista_produtos()
+
                 print("\033[;7m"+"\t\tA ►  Adição de produtos ao carrinho" +"\033[0;0m")
                 print("\t\tC ►  Finalização da venda do carrinho")
 
- #RAISSA
-                
-                produtos2 = [{'codigo': 1, 'nome': 'Jenipapo', 'preco': 25,
-                'codigo': 2, 'nome': 'Lichia', 'preco': 28,
-                'codigo': 3, 'nome': 'Pitaya', 'preco': 30,
-                'codigo': 4, 'nome': 'Kiwi', 'preco': 24,
-                'codigo': 5, 'nome': 'Goiaba', 'preco': 12,
-                'codigo': 6, 'nome': 'Pitanga', 'preco': 14,
-                'codigo': 7, 'nome': 'Café', 'preco': 20}]
-
-                #dicionário vazio do carrinho
-                carrinho = {}
-
+                #RAISSA
                 opcao = 0
                 flag_item_4A = True
 
                 while flag_item_4A == True:
-   
-                    os.system('cls')
                     print('===Carrinho de Compras=== \n')
                     print('1 - Adicionar Item')
                     print('2 - Remover Item')
                     print('3 - Ver itens do carrinho')
                     opcao = int(input('Digite a opção: '))
-    
-       
+
                     if opcao == 1:
-                        produto = input('Digite o nome do produto: ').title()
-                        quantidade = int(input('Digite quantidade: '))
-                        carrinho[produto] = quantidade
-                        flag_item_4A = False
-                        menu = input('Deseja voltar ao menu do carrinho de compras? S ou N: ').title()
-                        if menu == 'S':
-                            flag_item_4A = True
-        
-        
-                    if opcao == 2:
-                        print(carrinho)
-                        produto = input('Digite o nome do produto: ').title()
-                        carrinho.pop(produto)
-                        for key, value in carrinho.items():
-                            print(key, ' : ', carrinho[key])
+                        produto = produtos[int(input("Digite o ID do produto: "))].copy()
+                        produto["quantidade"] = int(input("Digite quantidade: "))
+                        carrinho.append(produto)
                         flag_item_4A = False
                         menu = input('Deseja voltar ao menu do carrinho de compras? S ou N: ').title()
                         if menu == 'S':
                             flag_item_4A = True
 
-    
-                    if opcao == 3:
-                        for key, value in carrinho.items():
-                            print(key, ' : ', carrinho[key])
+                    if opcao == 2:
+                        for produto in carrinho:
+                            print(carrinho.index(produto))
+                            print(produto)
+                        id = int(input('Digite o ID do produto: '))
+                        carrinho.pop(id)
                         flag_item_4A = False
                         menu = input('Deseja voltar ao menu do carrinho de compras? S ou N: ').title()
                         if menu == 'S':
                             flag_item_4A = True
-            
-            # FIM RAISSA #
+
+                    if opcao == 3:
+                        for produto in carrinho:
+                            print(carrinho.index(produto))
+                            print(produto)
+                        flag_item_4A = False
+                        menu = input('Deseja voltar ao menu do carrinho de compras? S ou N: ').title()
+                        if menu == 'S':
+                            flag_item_4A = True
+                # FIM RAISSA #
 
                 print("\n\t\tV ►  Voltar ao menu de Vendas")
                 print("\t\tS ►  S A I R   D O   S I S T E M A")
