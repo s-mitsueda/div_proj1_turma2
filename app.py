4bdadc339a9f45b69210764ad5a52f987603f971
Ettore Shiniti Mitsueda <ettore.mitsueda@luizalabs.com>
2022-10-18 15:08:48 -0300

    Padronizar função de adicionar ao carrinho

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
 from flask import Flask
-from flask import render_template
 from flask import url_for, redirect
 from flask import request
 import pandas as pd
 
-
 app = Flask(__name__)
 
-serie = pd.read_csv('produtosCarrinho.csv', header=None, names=['produtos', 'valores'])
-serie = serie.set_index('produtos')
-serie = serie['valores']
+serie = pd.read_csv('produtosCarrinho.csv')
 print(serie)
-
-carrinho = {}
 
 @app.route('/')
 def carrinhoDeCompras():
-    return 'Carrinho de Compras'
+    return redirect(url_for("static", filename="formulario.html"))
 
 @app.route('/comprar')
 def comprar():
     argumentos = request.args.to_dict()
-    qtd = argumentos['qtd']
-    nome = argumentos['produto']
-    serie[nome] = qtd
-    serie.to_csv('produtosCarrinho.csv', header=False)
-    print()
-    return argumentos
+    result = pd.concat([serie, pd.Series(argumentos).to_frame().T])
+    result.to_csv('produtosCarrinho.csv', index=False)
+
+    print(result)
+    return redirect(url_for("carrinhoDeCompras"))
 
 @app.route('/listar')
 def listar():
